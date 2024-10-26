65110b8626f3d96d822d0b5d5406209e4533b5d3
Luis Eduardo M. <luiseduardomc019@gmail.com>
2024-10-26 19:05:42 -0300

    implemented priority list

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// HuffNode is a BTreeNode & LinkedListNode
 typedef struct HuffNode_ {
   char data;
   int frequency;
@@ -10,9 +11,14 @@ typedef struct HuffNode_ {
   struct HuffNode_ *left;
   struct HuffNode_ *right;
 
-  struct HuffNode_ *next; // to be used in the list
+  struct HuffNode_ *next;
 } HuffNode;
 
+typedef struct LinkedList_ {
+  HuffNode *head;
+} LinkedList;
+
+// Creates a new node
 HuffNode *create_node(char ch, int freq) {
   HuffNode *newNode = (HuffNode *)malloc(sizeof(HuffNode));
   newNode->data = ch;
@@ -36,6 +42,7 @@ HuffNode *insert(HuffNode *root, char ch, int freq) {
   return root;
 }
 
+// Frees the memory allocated for a Binary Tree
 void free_tree(HuffNode *root) {
   if (root != NULL) {
     free_tree(root->left);
@@ -44,18 +51,31 @@ void free_tree(HuffNode *root) {
   }
 }
 
-void traversal(HuffNode *root) {
-  if (root != NULL) {
-    printf("Char: %c\tFreq: %d\n", root->data, root->frequency);
-    traversal(root->left);
-    traversal(root->right);
+// Frees the memory allocated for a Linked List
+void free_list(HuffNode *head) {
+  HuffNode *curr = NULL;
+  while (head != NULL) {
+    curr = head;
+    head = head->next;
+    free(curr);
   }
 }
 
+// Prints a Linked List
+void print_list(HuffNode *head) {
+  HuffNode *curr = head;
+  while (curr != NULL) {
+    printf("char: %c\t\tfreq: %d\n", curr->data, curr->frequency);
+    curr = curr->next;
+  }
+}
+
+// Checks if a node is a leaf
 bool is_leaf(HuffNode *node) {
   return (node->left == NULL && node->right == NULL);
 }
 
+// Compare the frequency between two nodes
 int compare_freq(HuffNode node1, HuffNode node2) {
   return node1.frequency - node2.frequency;
 }
@@ -87,37 +107,58 @@ FILE *file_handler() {
   return fp;
 }
 
-// char_frequency sets the frequency for each char in the file
-void char_frequency(int *freq) {
+// Sets the frequency for each char in the file
+void char_frequency(int *frequency) {
   FILE *fp = file_handler();
 
   int c;
   while ((c = fgetc(fp)) != EOF)
-    freq[c]++;
+    frequency[c]++;
 
   fclose(fp);
 }
 
-int main(void) {
-  HuffNode *root = NULL;
-  int freq[256] = {0};
+// Adds a node to the Linked List based on its frequency
+void insert_node(LinkedList *list, HuffNode *node) {
+  if (list->head == NULL) {
+    list->head = node;
+    return;
+  }
 
-  char_frequency(freq);
+  // Check if inserted node->freq is < head->freq
+  if (node->frequency < list->head->frequency) {
+    node->next = list->head;
+    list->head = node;
+    return;
+  }
+
+  HuffNode *curr = list->head;
+  while (curr->next != NULL && curr->next->frequency <= node->frequency) {
+    curr = curr->next;
+  }
+
+  // Insert node
+  node->next = curr->next;
+  curr->next = node;
+}
+
+int main(void) {
+  LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+  list->head = NULL;
+
+  int frequency[256] = {0};
+
+  char_frequency(frequency);
 
   for (int i = 0; i < 256; i++) {
-    if (freq[i] > 0) {
-      if (root == NULL)
-        root = create_node(i, freq[i]);
-      else
-        root = insert(root, i, freq[i]);
+    if (frequency[i] > 0) {
+      HuffNode *newNode = create_node(i, frequency[i]);
+      insert_node(list, newNode);
     }
   }
 
-  traversal(root);
-
-  printf("root: %c\t%d\n", root->data, root->frequency);
-
-  free(root);
+  print_list(list->head);
+  free_list(list->head);
 
   return 0;
 }
